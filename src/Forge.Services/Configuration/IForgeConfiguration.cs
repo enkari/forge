3a3c49afc1a35173071a5997c872cb676b7e271a
Nate Kohari <nate@enkari.com>
2009-09-01 18:03:13 -0400

    Buildout

---
+++ src/Forge.Services/Configuration/IForgeConfiguration.cs
@@ -0,0 +1,19 @@
+#region License
+// 
+// Author: Nate Kohari <nate@enkari.com>
+// Copyright (c) 2009, Enkari, Ltd.
+// 
+// Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+// See the file LICENSE.txt for details.
+// 
+#endregion
+#region Using Directives
+using System;
+#endregion
+
+namespace Forge.Services.Configuration
+{
+	public interface IForgeConfiguration
+	{
+	}
+}
