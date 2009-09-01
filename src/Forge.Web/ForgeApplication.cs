a40ec73b6382fde62039559d399a1bcf117695ca
Nate Kohari <nate@enkari.com>
2009-09-01 17:54:46 -0400

    Infrastructure

---
+++ src/Forge.Web/ForgeApplication.cs
@@ -0,0 +1,44 @@
+#region License
+// 
+// Author: Nate Kohari <nate@enkari.com>
+// Copyright (c) 2009, Enkari, Ltd.
+// 
+// Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+// See the file LICENSE.txt for details.
+// 
+#endregion
+#region Using Directives
+using System;
+using CommonServiceLocator.NinjectAdapter;
+using Forge.Web.Framework;
+using Microsoft.Practices.ServiceLocation;
+using Ninject;
+using Ninject.Web.Mvc;
+#endregion
+
+namespace Forge.Web
+{
+	public class ForgeApplication : NinjectHttpApplication
+	{
+		protected override void OnApplicationStarted()
+		{
+			var serviceLocator = new NinjectServiceLocator(Kernel);
+			ServiceLocator.SetLocatorProvider(() => serviceLocator);
+
+			RegisterAllControllersIn("Forge.Web");
+
+			var bootstrapper = Kernel.Get<Bootstrapper>();
+
+			bootstrapper.SetUpViewEngine();
+			bootstrapper.RegisterRoutes();
+		}
+
+		protected override IKernel CreateKernel()
+		{
+			var kernel = new StandardKernel();
+
+
+			return kernel;
+		}
+	}
+}
---
+++ src/Forge.Web/Framework/ForgeControllerBase.cs
@@ -0,0 +1,42 @@
+#region License
+// 
+// Author: Nate Kohari <nate@enkari.com>
+// Copyright (c) 2009, Enkari, Ltd.
+// 
+// Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+// See the file LICENSE.txt for details.
+// 
+#endregion
+#region Using Directives
+using System;
+using System.Web;
+using System.Web.Mvc;
+using Ninject.Website.Framework.Filters;
+#endregion
+
+namespace Forge.Web.Framework
+{
+	[HandleExceptions]
+	public abstract class ForgeControllerBase : Controller
+	{
+		public EmptyResult OK()
+		{
+			return new EmptyResult();
+		}
+
+		public HttpException NotFound()
+		{
+			return HttpExceptionFactory.NotFound();
+		}
+
+		public HttpException BadRequest()
+		{
+			return HttpExceptionFactory.BadRequest();
+		}
+
+		public HttpException Forbidden()
+		{
+			return HttpExceptionFactory.Forbidden();
+		}
+	}
+}
